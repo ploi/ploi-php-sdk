08082794a520f08c3883f7ae23a3edcf3780a45c
Dennis Smink <dennissmink@gmail.com>
2018-08-13 17:41:15 +0200

    Basic start for the core

--- src/Ploi.php
+++ src/Ploi.php
@@ -2,7 +2,40 @@
 
 namespace Ploi;
 
+use GuzzleHttp\Client;
+
 class Ploi
 {
+    /**
+     * Guzzle Client
+     *
+     * @var \GuzzleHttp\Client
+     */
+    public $guzzle;
+
+    /**
+     * Base API url
+     *
+     * @var $url
+     */
+    public $url = 'https://ploi.io/api';
+
+    /**
+     * Ploi constructor.
+     *
+     * @param $token
+     */
+    public function __construct($token)
+    {
+        $this->guzzle = new Client([
+            'base_uri' => $this->url,
+            'http_errors' => false,
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json'
+            ]
+        ]);
+    }
 
 }
